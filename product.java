ca0ce6263bdadcafc129b6884cab257c71770ef7
aditya rout <routaditya631@gmail.com>
2025-01-29 08:33:42 +0530

    some product details will given

---
+++ product.java
@@ -0,0 +1,14 @@
+class product
+{
+	int pid = 100;
+	String pname = "facewash";
+	double pprice =100;
+
+    public static void main(String[] args) 
+	{
+		product p = new product();
+		System.out.println(p.pid);
+		System.out.println(p.pname);
+		System.out.println(p.pprice);
+	}
+}
