77b390111dfd59e52059e071eada6411a79db3b1
Bapunu <routaditya631@gmail.com>
2025-04-17 18:47:48 +0530

    important

---
+++ Student.java
@@ -0,0 +1,24 @@
+package Aditya;
+
+class Student 
+{
+	int sid;
+	String sname;
+
+	public Student(int sid,String sname){
+		this.sid=sid;
+		this.sname=sname;
+	}
+	public String toString(){
+		return sid+" "+sname;
+	}
+	public static void main(String[] args) 
+	{
+		Student s1 = new Student(101,"mannu");
+		Student s2 = new Student(102,"sidharth");
+
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+}
